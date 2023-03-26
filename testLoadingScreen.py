2dd2b98e83e7903af6cec2eba85249e9b753ac74
Deven Allen <72590526+devenallen@users.noreply.github.com>
2023-03-26 10:31:24 -0400

    Add files via upload
    Two files that I have been working on. Getting the GUI ready for the initial loading screen for users to enter their username and password, and then more work on the Whiteboard part of this project.

---
+++ testLoadingScreen.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Login Window')
+
+        self.username_label = QLabel('Username:', self)
+        self.username_input = QLineEdit(self)
+
+        self.password_label = QLabel('Password:', self)
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.Password)
+
+        self.login_button = QPushButton('Login', self)
+        self.login_button.clicked.connect(self.login)
+
+        self.signup_button = QPushButton('Sign Up', self)
+        self.signup_button.clicked.connect(self.signup)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.username_label)
+        vbox.addWidget(self.username_input)
+        vbox.addWidget(self.password_label)
+        vbox.addWidget(self.password_input)
+        vbox.addWidget(self.login_button)
+        vbox.addWidget(self.signup_button)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 400, 200)
+
+    def login(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+        print(f'Username: {username}, Password: {password}')
+
+    def signup(self):
+        print('Sign up clicked')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    login_window = LoginWindow()
+    login_window.show()
+    sys.exit(app.exec_())
