2fafb10396cf18ea291cddc34d63149224ddce20
devenallen <devena4981@gmail.com>
2023-04-10 19:03:39 -0400

    Fixing rebasing issue

--- client/WBMenuScreen.py
+++ client/WBMenuScreen.py
@@ -24,26 +24,27 @@ class MainWindow(QWidget):
         self.setLayout(layout)
 
         # Get the list of active users from the server
-        users = self.get_active_users()
+        #users = self.get_active_users()
 
         # Update the label with the list of active users
-        self.label.setText('Active Users:\n' + '\n'.join(users))
+        #self.label.setText('Active Users:\n' + '\n'.join(users))
+        self.label.setText('Active Users:')
 
-    def get_active_users(self):
-        # Connect to the server and get the list of active users
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #s.connect(('server_address', 1234))
-            s.connect(('172.18.0.2', 1500))
-            s.sendall(b'get_active_users')
-            data = s.recv(1024)
-            s.close()
-            users = data.decode('utf-8').split('\n')
-            users = [u.strip() for u in users if u.strip()]
-            return users
-        except Exception as e:
-            print('Error:', e)
-            return []
+    # def get_active_users(self):
+    #     # Connect to the server and get the list of active users
+    #     try:
+    #         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #         #s.connect(('server_address', 1234))
+    #         s.connect(('172.18.0.2', 1500))
+    #         s.sendall(b'get_active_users')
+    #         data = s.recv(1024)
+    #         s.close()
+    #         users = data.decode('utf-8').split('\n')
+    #         users = [u.strip() for u in users if u.strip()]
+    #         return users
+    #     except Exception as e:
+    #         print('Error:', e)
+    #         return []
 
 
 if __name__ == '__main__':
